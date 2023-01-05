f46914beacf0a8911acf0b2083d3b6c65ff80e36
Jordan French <jtf981@culver.edu>
2023-01-05 09:31:35 -0600

    added axios stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+
 
 function App() {
+  const [image, setImage] = useState('');
+  useEffect(()=>{
+    axios.get('https://dog.ceo/api/breeds/image/random')
+    .then(response => {
+      setImage(response.data.message);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
+    // axios.post('https://reqres.in/api/user', {'name':"John", 'job':"programmer"})
+    // .then(response=>{
+    //   console.log(response.data)
+    // })
+    // .catch(error=>{
+    //   console.log(error)
+    // });
+    axios.get('https://reqres.in/api/users/2')
+      .then(response=>
+        console.log(response))
+      .catch(error =>
+        console.log(error));
+    
+    axios.put('https://reqres.in/api/users/2', {'name':"John", 'job':"baller"})
+      .then(response=>{
+        console.log(response.data)
+      })
+      .catch(error=>{
+        console.log(error)
+      });
+    
+    // axios.delete('https://reqres.in/api/users/2')
+    //   .then(response=>
+    //     console.log(response))
+    //   .catch(error=>
+    //     console.log(error));
+  },[]);
+  
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={image} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +57,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn 
         </a>
       </header>
     </div>
